faf261b946faac0206ee14530a412172b9f3f241
ASZXJOHN <q2381050@gmail.com>
2024-03-19 00:09:49 +0800

    Submodule建置成功

--- README.md
+++ README.md
@@ -1,2 +1,40 @@
-# JavaLearningDisplay-Submodule-Demo
+# JavaLearningDisplay
 Java Learning Display
+
+專案介紹
+自主練習展示
+此專案目標是將自己有興趣的東西以模組化的方式撰寫出來。
+並盡可能的去研究多人合作開發的話要怎麼讓工作更順利。
+Version : 0.2.0
+last updated date : 2024/03/12
+
+
+主要目標:
+1. 專案以multi-module的方式建置、Common與ORM要能被多個專案引用 (以完成)
+2. 專案要能支援git submodule 功能 (製作中)
+
+
+功能列表:
+1. 客戶帳號註冊系統、客戶帳號審核系統，專案名稱:RegAuthSystem  ( in progress 以實現功能:帳號註冊、查詢註冊進度)
+2. JWT登入系統，且使用Redies紀錄Token(Pending)
+3. 產品訂價管理系統(Pending)
+
+
+
+專案環境建置文件放置於/document/環境建置SOP.docx
+
+
+程式架構邏輯
+各種Controller > Controller同名Service > Entity同名Service > Repository >Entity
+
+專案引用邏輯
+各種System > orm > common > util工具專案(示意)
+
+好處是 orm 跟 common可以使用成為各種專案的基底，或同時支援多種專案，
+甚至製作util工具專案成為公司的library被不同的專案使用，也可以支援Git Submodule來增加靈活性
+
+
+
+未來待研究
+1. Kubernetes 自動擴展
+2. Doxygen 自動文件產生，用來快速生產系統文件讓維運更順暢
---
+++ document/demoDB.sql
@@ -0,0 +1,136 @@
+-- MySQL dump 10.13  Distrib 8.0.36, for Win64 (x86_64)
+--
+-- Host: localhost    Database: demo
+-- ------------------------------------------------------
+-- Server version	8.0.36
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `client`
+--
+
+DROP TABLE IF EXISTS `client`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `client` (
+  `client_id` bigint NOT NULL AUTO_INCREMENT COMMENT 'PK',
+  `client_info_id` bigint DEFAULT NULL COMMENT 'FK client_info.id。客戶個資',
+  `customer_number` varchar(20) DEFAULT NULL COMMENT '客戶編號10碼',
+  `email` varchar(255) NOT NULL COMMENT '客戶的Email，登入帳號',
+  `password` varchar(2000) DEFAULT '' COMMENT '客戶密碼',
+  `set_password_verification_code_email` varchar(36) DEFAULT NULL COMMENT '存放寄送給客戶的第一次建立密碼郵件中的驗證碼',
+  `set_password_verification_code_expiry_time` timestamp NULL DEFAULT NULL COMMENT 'set_password_verification_code_email的過期時間',
+  `reset_password_verification_code_email` varchar(36) DEFAULT NULL COMMENT '從有密碼後的自二次開始，每次改密碼都是透過次欄位處理\n',
+  `reset_password_verification_code_expiry_time` timestamp NULL DEFAULT NULL COMMENT 'reset_password_verification_code_email的過期時間',
+  `enable_two_factor` bit(1) NOT NULL DEFAULT b'1' COMMENT '1:true(啟用)、0:false(停用)\n啟用二階段驗證',
+  `two_factor_code` varchar(4) DEFAULT NULL COMMENT '二階段驗證碼',
+  `two_factor_code_expiry_time` timestamp NULL DEFAULT NULL COMMENT 'two_factor_code的過期時間',
+  `status` int NOT NULL DEFAULT '0' COMMENT '0:通過email驗證，尚未送出基本資料\n20:基本資料送出，待指派業務\n40:業務助理審核完成\n60:業務審核完成\n100:業務主管審核完成\n-1:遭到停用\n-99:遭到禁用',
+  `registration_progress_verification_code` varchar(36) DEFAULT NULL COMMENT '記錄使用者用來查詢註冊進度的驗證碼的欄位',
+  `registration_progress_verification_code_expiry_time` timestamp NULL DEFAULT NULL COMMENT 'registration_progress_verification_code的過期時間\n',
+  `login_success_date` timestamp NULL DEFAULT NULL COMMENT '最後成功登入時間',
+  `login_fail_count` int DEFAULT NULL COMMENT '登入失敗次數記錄',
+  `is_lock` bit(1) DEFAULT b'0' COMMENT '帳號是否因為登入失敗次數過多被鎖起來\n預設0:FALSE 沒有被鎖、1:TRUE 有被鎖\n',
+  `is_locked_time` varchar(45) DEFAULT NULL COMMENT '被帳號鎖住的時間',
+  `register_review_id` bigint DEFAULT NULL COMMENT '註冊審核紀錄表\n',
+  `create_user` varchar(45) NOT NULL,
+  `update_user` varchar(45) NOT NULL,
+  `create_date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `update_date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`client_id`),
+  UNIQUE KEY `email_UNIQUE` (`email`),
+  UNIQUE KEY `client_number_UNIQUE` (`customer_number`)
+) ENGINE=InnoDB AUTO_INCREMENT=18 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `client`
+--
+
+LOCK TABLES `client` WRITE;
+/*!40000 ALTER TABLE `client` DISABLE KEYS */;
+INSERT INTO `client` VALUES (17,NULL,NULL,'q2381050@gmail.com','',NULL,NULL,NULL,NULL,_binary '',NULL,NULL,5,'e6fc5948-d316-4eb4-8607-01bf3475ca7f','2024-03-15 15:56:50',NULL,0,_binary '\0',NULL,NULL,'system','system','2024-03-14 15:56:50','2024-03-14 15:56:50');
+/*!40000 ALTER TABLE `client` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `email_template`
+--
+
+DROP TABLE IF EXISTS `email_template`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `email_template` (
+  `email_template_id` bigint NOT NULL AUTO_INCREMENT,
+  `associated_api` varchar(45) NOT NULL,
+  `subject` varchar(255) DEFAULT NULL COMMENT '信件主旨',
+  `content` text COMMENT '信件內容',
+  `create_user` varchar(45) DEFAULT NULL,
+  `update_user` varchar(45) NOT NULL,
+  `create_date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `update_date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`email_template_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci COMMENT='存放寄信用的信件範本';
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `email_template`
+--
+
+LOCK TABLES `email_template` WRITE;
+/*!40000 ALTER TABLE `email_template` DISABLE KEYS */;
+INSERT INTO `email_template` VALUES (1,'register_user','使用者註冊信件','您好，\r\n \r\n 此電子郵件已由是蔡岳翰的測試系統自動發送，用於進入註冊頁面填寫基本資料。\r\n 請使用下方的連結填寫你的基本資料。\r\n   \r\n   {{URL-Register}}\r\n   \r\n Greeting,\r\n This email has been automatically sent by Chief Telecom and is used to enter the registration page to fill in the basic information.\r\n Please use the link below to fill in your basic information.\r\n \r\n   {{URL-Register}}','system','system','2024-03-14 12:57:09','2024-03-14 12:57:09');
+/*!40000 ALTER TABLE `email_template` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `system_parameter_setting`
+--
+
+DROP TABLE IF EXISTS `system_parameter_setting`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `system_parameter_setting` (
+  `setting_id` bigint NOT NULL AUTO_INCREMENT COMMENT '設定檔ID',
+  `system_group` varchar(255) DEFAULT NULL COMMENT '系統群組代號\n',
+  `system_key` varchar(255) DEFAULT NULL COMMENT '系統群組中定位或使用情境\n',
+  `system_value` varchar(255) DEFAULT NULL COMMENT '參數內容\n',
+  `system_text` text COMMENT '參數內容，當value不夠時才需要使用\n',
+  `create_user` varchar(45) DEFAULT NULL,
+  `update_user` varchar(45) DEFAULT NULL,
+  `create_date` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  `update_date` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`setting_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci COMMENT='系統參數設定';
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `system_parameter_setting`
+--
+
+LOCK TABLES `system_parameter_setting` WRITE;
+/*!40000 ALTER TABLE `system_parameter_setting` DISABLE KEYS */;
+INSERT INTO `system_parameter_setting` VALUES (1,'email','email_expiration_time','86400','信件的有效期間','system','system','2024-03-14 08:12:52','2024-03-14 08:12:52'),(2,'email','email_sender','Shirai <shiraiforwork@gmail.com>','信件的寄信者','system','system','2024-03-14 13:31:18','2024-03-14 13:31:18'),(3,'email','email_type_register_user','http://127.0.0.1:8080/user/','註冊信件裡面附上的 URL，使用會被導向填入個資的頁面','system','system','2024-03-14 14:53:26','2024-03-14 14:53:26');
+/*!40000 ALTER TABLE `system_parameter_setting` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2024-03-15  0:55:30
