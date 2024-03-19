1a70a84cd6eff3f311cd4a8298a5a7190a5761f8
ASZXJOHN <q2381050@gmail.com>
2024-03-19 08:48:51 +0800

    更新SQL

--- document/demoDB.sql
+++ document/demoDB.sql
@@ -38,19 +38,23 @@ CREATE TABLE `client` (
   `status` int NOT NULL DEFAULT '0' COMMENT '0:通過email驗證，尚未送出基本資料\n20:基本資料送出，待指派業務\n40:業務助理審核完成\n60:業務審核完成\n100:業務主管審核完成\n-1:遭到停用\n-99:遭到禁用',
   `registration_progress_verification_code` varchar(36) DEFAULT NULL COMMENT '記錄使用者用來查詢註冊進度的驗證碼的欄位',
   `registration_progress_verification_code_expiry_time` timestamp NULL DEFAULT NULL COMMENT 'registration_progress_verification_code的過期時間\n',
+  `registration_verification_code` varchar(36) DEFAULT NULL COMMENT '記錄註冊信件的驗證碼的欄位',
+  `registration_verification_code_expiry_time` timestamp NULL DEFAULT NULL COMMENT '記錄註冊信件的驗證碼的過期時間',
   `login_success_date` timestamp NULL DEFAULT NULL COMMENT '最後成功登入時間',
   `login_fail_count` int DEFAULT NULL COMMENT '登入失敗次數記錄',
   `is_lock` bit(1) DEFAULT b'0' COMMENT '帳號是否因為登入失敗次數過多被鎖起來\n預設0:FALSE 沒有被鎖、1:TRUE 有被鎖\n',
   `is_locked_time` varchar(45) DEFAULT NULL COMMENT '被帳號鎖住的時間',
   `register_review_id` bigint DEFAULT NULL COMMENT '註冊審核紀錄表\n',
-  `create_user` varchar(45) NOT NULL,
-  `update_user` varchar(45) NOT NULL,
+  `create_user` varchar(45) NOT NULL DEFAULT 'system',
+  `update_user` varchar(45) NOT NULL DEFAULT 'system',
   `create_date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `update_date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`client_id`),
   UNIQUE KEY `email_UNIQUE` (`email`),
-  UNIQUE KEY `client_number_UNIQUE` (`customer_number`)
-) ENGINE=InnoDB AUTO_INCREMENT=18 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+  UNIQUE KEY `client_number_UNIQUE` (`customer_number`),
+  UNIQUE KEY `set_password_verification_code_email_UNIQUE` (`set_password_verification_code_email`),
+  UNIQUE KEY `reset_password_verification_code_email_UNIQUE` (`reset_password_verification_code_email`)
+) ENGINE=InnoDB AUTO_INCREMENT=29 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -59,7 +63,7 @@ CREATE TABLE `client` (
 
 LOCK TABLES `client` WRITE;
 /*!40000 ALTER TABLE `client` DISABLE KEYS */;
-INSERT INTO `client` VALUES (17,NULL,NULL,'q2381050@gmail.com','',NULL,NULL,NULL,NULL,_binary '',NULL,NULL,5,'e6fc5948-d316-4eb4-8607-01bf3475ca7f','2024-03-15 15:56:50',NULL,0,_binary '\0',NULL,NULL,'system','system','2024-03-14 15:56:50','2024-03-14 15:56:50');
+INSERT INTO `client` VALUES (28,NULL,NULL,'q2381050@gmail.com','',NULL,NULL,NULL,NULL,_binary '',NULL,NULL,5,NULL,NULL,'b633b02c-e53f-44a0-9c14-7daaf0ade195','2024-03-19 17:03:59',NULL,0,_binary '\0',NULL,NULL,'system','system','2024-03-18 17:04:01','2024-03-18 17:04:01');
 /*!40000 ALTER TABLE `client` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -94,6 +98,37 @@ INSERT INTO `email_template` VALUES (1,'register_user','使用者註冊信件','
 UNLOCK TABLES;
 
 --
+-- Table structure for table `error_message`
+--
+
+DROP TABLE IF EXISTS `error_message`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `error_message` (
+  `error_message_id` bigint NOT NULL AUTO_INCREMENT,
+  `message_group` varchar(255) NOT NULL COMMENT '系統群組代號',
+  `message_key` varchar(255) NOT NULL COMMENT '系統群組中定位或使用情境',
+  `usage_scenarios` varchar(255) DEFAULT NULL COMMENT '使用情況說明',
+  `message_text` text COMMENT '參數內容',
+  `create_user` varchar(45) DEFAULT 'system',
+  `update_user` varchar(45) DEFAULT 'system',
+  `create_date` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  `update_date` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`error_message_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `error_message`
+--
+
+LOCK TABLES `error_message` WRITE;
+/*!40000 ALTER TABLE `error_message` DISABLE KEYS */;
+INSERT INTO `error_message` VALUES (1,'register_or_validate_user','initiate_registration_with_email','failed_to_send_letter','initiateRegistrationWithEmail is fail, because:','system','system','2024-03-16 08:50:21','2024-03-16 08:50:21'),(2,'register_or_validate_user','is_the_verification_letter_valid','invalid','The last verification letter is still valid, so cancel sending again.','system','system','2024-03-16 08:50:21','2024-03-16 08:50:21');
+/*!40000 ALTER TABLE `error_message` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
 -- Table structure for table `system_parameter_setting`
 --
 
@@ -102,12 +137,12 @@ DROP TABLE IF EXISTS `system_parameter_setting`;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `system_parameter_setting` (
   `setting_id` bigint NOT NULL AUTO_INCREMENT COMMENT '設定檔ID',
-  `system_group` varchar(255) DEFAULT NULL COMMENT '系統群組代號\n',
-  `system_key` varchar(255) DEFAULT NULL COMMENT '系統群組中定位或使用情境\n',
-  `system_value` varchar(255) DEFAULT NULL COMMENT '參數內容\n',
-  `system_text` text COMMENT '參數內容，當value不夠時才需要使用\n',
-  `create_user` varchar(45) DEFAULT NULL,
-  `update_user` varchar(45) DEFAULT NULL,
+  `system_group` varchar(255) DEFAULT NULL COMMENT '系統群組代號',
+  `system_key` varchar(255) DEFAULT NULL COMMENT '系統群組中定位或使用情境',
+  `system_value` varchar(255) DEFAULT NULL COMMENT '參數內容',
+  `system_text` text COMMENT '參數說明',
+  `create_user` varchar(45) DEFAULT 'system',
+  `update_user` varchar(45) DEFAULT 'system',
   `create_date` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
   `update_date` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`setting_id`)
@@ -120,7 +155,7 @@ CREATE TABLE `system_parameter_setting` (
 
 LOCK TABLES `system_parameter_setting` WRITE;
 /*!40000 ALTER TABLE `system_parameter_setting` DISABLE KEYS */;
-INSERT INTO `system_parameter_setting` VALUES (1,'email','email_expiration_time','86400','信件的有效期間','system','system','2024-03-14 08:12:52','2024-03-14 08:12:52'),(2,'email','email_sender','Shirai <shiraiforwork@gmail.com>','信件的寄信者','system','system','2024-03-14 13:31:18','2024-03-14 13:31:18'),(3,'email','email_type_register_user','http://127.0.0.1:8080/user/','註冊信件裡面附上的 URL，使用會被導向填入個資的頁面','system','system','2024-03-14 14:53:26','2024-03-14 14:53:26');
+INSERT INTO `system_parameter_setting` VALUES (1,'email','email_expiration_time','86400','信件的有效期間','system','system','2024-03-14 08:12:52','2024-03-14 08:12:52'),(2,'email','email_sender','Shirai <shiraiforwork@gmail.com>','信件的寄信者','system','system','2024-03-14 13:31:18','2024-03-14 13:31:18'),(3,'email','email_type_register_user','http://127.0.0.1:8080/user/register/','註冊信件裡面附上的 URL，使用會被導向填入個資的頁面','system','system','2024-03-14 14:53:26','2024-03-14 14:53:26');
 /*!40000 ALTER TABLE `system_parameter_setting` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -133,4 +168,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2024-03-15  0:55:30
+-- Dump completed on 2024-03-19  8:47:22
